b5c98b92a553ecb008cfa763af1cc2e62d23b3c7
Laksamana-akun <laksamanadd@gmail.com>
2026-04-28 08:16:05 +0700

    Aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,15 @@
+﻿using Modul9_103022400139;
+using System;
+using System.IO;
+using System.Text.Json;
+
+public class BankTransferConfig
+{
+    private const string ConfigFilePath = "bank_transfer_config.json";
+
+    public string lang { get; set; } = "en";
+
+    public Transfer transfer { get; set; }
+    public string[] methods { get; set; }
+    public Confirmation confirmation { get; set; }
+}
---
+++ Transfer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul9_103022400139
+{
+    public class Transfer
+    {
+        public int treshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+        public Transfer(){}
+        
+        public Transfer(int treshold, int low_fee, int high_fee)
+        {
+            this.treshold = treshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+}
